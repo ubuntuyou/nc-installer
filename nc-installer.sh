21a4e45cfa109652bd6ead79e46f03b6b5483734
ubuntuyou <43767473+ubuntuyou@users.noreply.github.com>
2025-10-06 15:21:38 -0500

    Update nc-installer.sh

--- nc-installer.sh
+++ nc-installer.sh
@@ -166,8 +166,8 @@ endmsg
 sed -i 's/memory\_limit \= 128M/memory\_limit \= 512M/g' /etc/php/${PHPVER}/fpm/php.ini
 sed -i 's/upload_max_filesize = 2M/upload_max_filesize = 2G/g' /etc/php/${PHPVER}/fpm/php.ini
 sed -i 's/max_file_uploads = 20/max_file_uploads = 200/g' /etc/php/${PHPVER}/fpm/php.ini
-sed -i "s/;opcache.interned_strings_buffer=8/opcache.interned_strings_buffer=16/g" /etc/php/8.3/fpm/php.ini
-sed -i "s/;opcache.interned_strings_buffer=8/opcache.interned_strings_buffer=16/g" /etc/php/8.3/cli/php.ini
+sed -i "s/;opcache.interned_strings_buffer=8/opcache.interned_strings_buffer=16/g" /etc/php/${PHPVER}/fpm/php.ini
+sed -i "s/;opcache.interned_strings_buffer=8/opcache.interned_strings_buffer=16/g" /etc/php/${PHPVER}/cli/php.ini
 
 sed -i 's/;env/env/g' /etc/php/${PHPVER}/fpm/pool.d/www.conf
 
